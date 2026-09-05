c8ddf56c15578d29a149eda73f27def2160a6c7c
Pete <16416542+theGreenJedi@users.noreply.github.com>
2026-09-05 13:44:03 -0400

    Bump alpha polish build to 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "org.petesreps"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.5.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
